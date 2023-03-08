bd55b8c63de1d9a79f8dd1021637c306beb2962b
Ryo Martin <laetuzg@gmail.com>
2023-03-08 23:12:01 +0700

    Merged + removed redundant code

--- app/src/main/java/com/neotica/viewpager2/MainActivity.kt
+++ app/src/main/java/com/neotica/viewpager2/MainActivity.kt
@@ -3,7 +3,6 @@ package com.neotica.viewpager2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.neotica.viewpager2.databinding.ActivityMainBinding
 
